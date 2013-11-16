a53618b3bf45d4a3cae9fe8eeb681e4f6ddce225
brianthecoder <brian@20jeans.com>
2013-11-15 18:58:08 -0800

    cleanup and refactor

--- app/models/spree/relation.rb
+++ app/models/spree/relation.rb
@@ -1,7 +1,11 @@
 class Spree::Relation < ActiveRecord::Base
   belongs_to :relation_type
-  belongs_to :relatable, :polymorphic => true
-  belongs_to :related_to, :polymorphic => true
+  belongs_to :relatable, polymorphic: true
+  belongs_to :related_to, polymorphic: true
 
-  validates :relation_type, :relatable, :related_to, :presence => true
+  validates :relation_type, :relatable, :related_to, presence: true
+
+  def product_discount(product)
+    where("discount_amount <> 0.0 AND relatable_type = ? AND relatable_id = ?", "Spree::Product", product.id)
+  end
 end
